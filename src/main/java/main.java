074504244075b741f3dfaeb8c5f75be5435eab2b
Flow <flo-m@gmx.at>
2021-03-12 10:25:52 +0100

    Repo forked and basic structure added, maven Project

---
+++ src/main/java/main.java
@@ -0,0 +1,7 @@
+public class main {
+
+    public static void main(String[] args){
+        System.out.println("prog2-ss21-exercise1");
+
+    }
+}
---
+++ src/test/java/ResultTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ResultTest {
+
+    @BeforeAll
+    static void init() {
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @DisplayName("First test scenario")
+    @Test
+    void test_1(){
+    }
+
+}
