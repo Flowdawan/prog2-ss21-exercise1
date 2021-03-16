5d5c6fb6598fabf55e55aea4ae91b4bf67f12181
Flow <flo-m@gmx.at>
2021-03-16 21:12:31 +0100

    Added check for grades are not of same length with provided students in the Result class.

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class Result {
 
     public static List<Integer> gradingStudents(List<Integer> grades) {
-        if(grades == null || grades.isEmpty() || grades.get(0) < 1 || grades.get(0) > 60){
+        if(grades == null || grades.isEmpty() || grades.get(0) < 1 || grades.get(0) > 60 || grades.get(0) != grades.toArray().length -1){
             System.out.println("Please check the provided list! Something is wrong with the list!");
             return null;
         }
