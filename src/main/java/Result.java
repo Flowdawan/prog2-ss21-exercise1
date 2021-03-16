99f568832971c11b4fd6e6c3a94d3473598a174f
Flow <flo-m@gmx.at>
2021-03-16 20:45:46 +0100

    Implemented basic functionality to gradingStudents method, checks now for invalid inputs by grades and number of students regarding to test cases.

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -3,6 +3,15 @@ import java.util.List;
 public class Result {
 
     public static List<Integer> gradingStudents(List<Integer> grades) {
+        if(grades.get(0) < 1 || grades.get(0) > 60 || grades.isEmpty()){
+            return null;
+        }
+        for(int value : grades){
+            if(value < 0 || value > 0){
+                return null;
+            }
+        }
+
         return grades;
     }
 }
